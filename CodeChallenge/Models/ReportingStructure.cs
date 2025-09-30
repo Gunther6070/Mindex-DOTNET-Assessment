225e25b2b6c2157c4a95e18a00bff822522d7b6b
hayden <haydenhartman10@gmail.com>
2025-09-30 10:26:42 -0400

    Implemented ReportingStructure.cs class, creating helper method to calculate NumberOfReports

---
+++ CodeChallenge/Models/ReportingStructure.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+
+namespace CodeChallenge.Models;
+
+public class ReportingStructure
+{
+    public Employee Employee { get; set; }
+    public int NumberOfReports { get => CountReports(Employee.DirectReports);}
+    
+    /// <summary>
+    /// Calculates the number of employees that report to a given employee
+    /// </summary>
+    /// <param name="reporters">
+    /// A list of employees
+    /// </param>
+    /// <returns>
+    /// The number of employees in a reporting structure
+    /// </returns>
+    private static int CountReports(List<Employee> reporters)
+    {
+        var sum = 0;
+        if (reporters.Count == 0)
+        {
+            return 0;
+        }
+        
+        foreach (var employee in reporters)
+        {
+            sum += CountReports(employee.DirectReports) + 1;
+        }
+
+        return sum;
+    }
+    
+}
